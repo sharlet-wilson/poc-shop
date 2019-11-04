db6a95bfdd9d250b008cf9bc1708a4c9c515eae1
Sharlet Hannah Wilson <sharlet.wilson@gmail.com>
2019-11-04 13:02:26 +0530

    Fetch product list from API

--- src/shared/store/actions/index.js
+++ src/shared/store/actions/index.js
@@ -1,11 +1,12 @@
 import { all, call, delay, put, takeEvery } from 'redux-saga/effects';
+import axios from 'axios';
 
 export const FETCH_PRODUCTS = 'FETCH_PRODUCTS';
 export const FETCH_PRODUCTS_SUCCESS = 'FETCH_PRODUCTS_SUCCESS';
 
 export function* fetchProducts() {
-  yield delay(1000);
-  yield put({ type: FETCH_PRODUCTS_SUCCESS, data: [{ id: 1, name: 'Test' }, { id: 2, name: 'Test2' }] })
+  const { data } = yield call(() => axios.get('https://poc-shop.free.beeceptor.com/products'))
+  yield put({ type: FETCH_PRODUCTS_SUCCESS, data })
 }
 
 export function* watchFetchProducts() {
